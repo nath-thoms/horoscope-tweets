6eecd8ec157f5ca507a0864902aeb58385461318
Nath Chadwick <nath.chadwick@gmail.com>
2018-04-06 16:08:59 +0100

    prep for heroku deployment

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.get('/profile', getStarSign);
 
 
 app.use(bodyParser.json());
-// app.use('/*', (req, res, next) => next({ status: 404 }))
+app.use('/*', (req, res, next) => next({ status: 404 }))
 
 
-// app.use((err, req, res, next) => {
-//     if (err.status === 400) res.status(400).send({ message: 'Bad request' });
-//     else if (err.status = 404) res.status(404).send({ message: err });
-//     else res.status(500).send({ message: 'Internal server error' })
-// });
+app.use((err, req, res, next) => {
+    if (err.status === 400) res.status(400).send({ message: 'Bad request' });
+    else if (err.status = 404) res.status(404).send({ message: err });
+    else res.status(500).send({ message: 'Internal server error' })
+});
 
 module.exports = app;
--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const PORT = process.env.PORT || 9090;
 
-app.listen(9090, () => {
-    console.log('listening on port 9090')
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })
