aca995258956a587f4f39cb8f2f07dc474bc86d6
Nath Chadwick <nath.chadwick@gmail.com>
2018-04-06 11:37:28 +0100

    added some ejs and styling

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const { getStarSign } = require('./controllers');
 
-app.use(bodyParser.json());
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+    res.render('pages/home')
+})
+
 app.get('/:twit_name', getStarSign);
 
 
-app.use('/*', (req, res, next) => next({ status: 404 }))
+app.use(bodyParser.json());
+// app.use('/*', (req, res, next) => next({ status: 404 }))
 
 
-app.use((err, req, res, next) => {
-    if (err.status === 400) res.status(400).send({ message: 'Bad request' });
-    else if (err.status = 404) res.status(404).send({ message: err });
-    else res.status(500).send({ message: 'Internal server error' })
-});
+// app.use((err, req, res, next) => {
+//     if (err.status === 400) res.status(400).send({ message: 'Bad request' });
+//     else if (err.status = 404) res.status(404).send({ message: err });
+//     else res.status(500).send({ message: 'Internal server error' })
+// });
 
 module.exports = app;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const PersonalityInsights = require('watson-developer-cloud/personality-insights/v3');
 const Twit = require('twit');
 const { twitterKeys, personalityConfig } = require('../config');
+const fs = require('fs')
 
 const pi = new PersonalityInsights({
     username: personalityConfig.username,
@@ -47,65 +48,72 @@ function getPersonalityInsight(content) {
 function getStarSign(req, res, next) {
     let twit_name = req.params.twit_name;
     getTweets(twit_name)
-    .then(tweetText => {
-        if (tweetText.join('').split(' ').length < 300) return null;
-        else return getPersonalityInsight(JSON.stringify(tweetText))
-    })
-    .then(info => {
-        //emotional range
-        if (info.personality[4].percentile < 0.2) {
-            res.send('aquarius')
-        }
-        //cautiousness
-        else if (info.personality[1].children[1].percentile > 0.75) {
-            res.send('pisces')
-        }
-        //fiery
-        else if (info.personality[4].children[0].percentile > 0.85) {
-            res.send('aries')
-        }
-        //openess to change
-        else if (info.values[1].percentile < 0.15) {
-            res.send('taurus')
-        }
-        //prone to worry
-        else if (info.personality[4].children[1].percentile > 0.90) {
-            res.send('gemini')
-        }
-        //cheerfulness
-        else if (info.personality[2].children[2].percentile < 0.30) {
-            res.send('cancer')
-        }
-        //Conscientiousness
-        else if (info.personality[1].percentile < 0.10) {
-            res.send('leo')
-        }
-        //outgoing
-        else if (info.personality[2].children[4].percentile < 0.30) {
-            res.send('virgo')
-        }
-        //assertiveness
-        else if (info.personality[2].children[1].percentile < 0.2) {
-            res.send('libra')
-        }
-        //trust
-        else if (info.personality[3].children[5].percentile < 0.33) {
-            res.send('scorpio')
-        }
-        //intellect
-        else if (info.personality[0].children[4].percentile < 0.10) {
-            res.send('sagitarius')
-        }
-        //modesty
-        else if (info.personality[3].children[2].percentile < 0.10) {
-            res.send('capricorn')
-        }
+        .then(tweetText => {
+            if (tweetText.join('').split(' ').length < 300) return null;
+            else return getPersonalityInsight(JSON.stringify(tweetText))
+        })
+        .then(info => {
+            //emotional range aquarius
+            if (info.personality[4].percentile < 0.2) {
+                return matchStarSign(0, res)
+            }
+            //cautiousness pisces
+            else if (info.personality[1].children[1].percentile > 0.75) {
+                return matchStarSign(1, res)
+            }
+            //fiery aries
+            else if (info.personality[4].children[0].percentile > 0.85) {
+                return matchStarSign(2, res)
+            }
+            //openess to change taurus
+            else if (info.values[1].percentile < 0.15) {
+                return matchStarSign(3, res)
+            }
+            //prone to worry gemini
+            else if (info.personality[4].children[1].percentile > 0.90) {
+                return matchStarSign(4, res)
+            }
+            //cheerfulness cancer
+            else if (info.personality[2].children[2].percentile < 0.30) {
+                return matchStarSign(5, res)
+            }
+            //Conscientiousness leo
+            else if (info.personality[1].percentile < 0.10) {
+                return matchStarSign(6, res)
+            }
+            //assertiveness libra
+            else if (info.personality[2].children[1].percentile < 0.2) {
+                return matchStarSign(7, res)
+            }
+            //outgoing virgo
+            else if (info.personality[2].children[4].percentile < 0.30) {
+                return matchStarSign(8, res)
+            }
+            //trust scorpio
+            else if (info.personality[3].children[5].percentile < 0.33) {
+                return matchStarSign(9, res)
+            }
+            //intellect sagitarius
+            else if (info.personality[0].children[4].percentile < 0.10) {
+                return matchStarSign(10, res)
+            }
+            //modesty capricorn
+            else if (info.personality[3].children[2].percentile < 0.10) {
+                return matchStarSign(11, res)
+            }
         })
         .catch(next)
 }
 
-function matchStarSign (){
-
+function matchStarSign(starSign, res) {
+    fs.readFile(`${process.cwd()}/db/data.json`, "utf-8", (err, data) => {
+        if (err) {
+            console.log({ error: err + 'in get Star Sign function' })
+        } else {
+            data = JSON.parse(data)
+            res.render('pages/horoscope', { horoscope: data[starSign] })
+        }
+    })
 }
 
 
