f8541f6aebd2ffbbe1af831eeb1dd7c42aa9310a
Nath Chadwick <nath.chadwick@gmail.com>
2018-04-06 12:14:58 +0100

    linked css styling

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const bodyParser = require('body-parser');
 const { getStarSign } = require('./controllers');
 
 app.set('view engine', 'ejs')
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
     res.render('pages/home')
 })
